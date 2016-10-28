9dbee0bad2f381fe2556396106f7824b6eb2f3c5
chrisarndt <arndt_c@ymail.com>
2016-10-28 15:13:24 +0200

    Définition de la base de données. C'est à partir de cette classe que les tables et colonnes vont être générées par EntityFramework

---
+++ Madera.Data/MaderaEntities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Madera.Data.Configuration;
+using Madera.Model;
+
+namespace Madera.Data
+{
+    public class MaderaEntities : DbContext
+    {
+        public MaderaEntities() : base("Madera") { }
+
+        public DbSet<Adresse> Adresses { get; set; }
+
+        public virtual void Commit()
+        {
+            base.SaveChanges();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AdresseConfiguration());
+        }
+    }
+}
