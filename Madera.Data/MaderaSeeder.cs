fe60e69dab5e1b8cf8e602c9b379f7ed5d8f1e33
chrisarndt <arndt_c@ymail.com>
2016-10-28 15:14:31 +0200

    Ajout d'une classe MaderaSeeder. Au démarrage de l'application si la base de données est vierge, les données contenues dans cette classe y seront chargées.

---
+++ Madera.Data/MaderaSeeder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Madera.Model;
+
+namespace Madera.Data
+{
+    public class MaderaSeeder : DropCreateDatabaseIfModelChanges<MaderaEntities>
+    {
+        protected override void Seed(MaderaEntities context)
+        {
+            GetCategories().ForEach(c => context.Adresses.Add(c));
+
+            context.Commit();
+        }
+
+        private static List<Adresse> GetCategories()
+        {
+            return new List<Adresse>
+            {
+                new Adresse {
+                    codePostal = "54000",
+                    ville = "Nancy",
+                    pays = "France",
+                    numRue = "2bis",
+                    nomRue = "Jean Jacques"
+                },
+                new Adresse {
+                    codePostal = "54000",
+                    ville = "Nancy",
+                    pays = "France",
+                    numRue = "14",
+                    nomRue = "Cours Léopold"
+                }
+            };
+        }
+    }
+}
