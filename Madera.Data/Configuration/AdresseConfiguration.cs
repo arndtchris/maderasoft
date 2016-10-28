85d4feeaa555201e26cd9773a874656040e01998
chrisarndt <arndt_c@ymail.com>
2016-10-28 15:11:45 +0200

    Ajout d'une classe AdresseConfiguration permettant de mapper la classe métier Adresse

---
+++ Madera.Data/Configuration/AdresseConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using Madera.Model;
+
+namespace Madera.Data.Configuration
+{
+    public class AdresseConfiguration : EntityTypeConfiguration<Adresse>
+    {
+        public AdresseConfiguration()
+        {
+            ToTable("Adresse");
+            HasKey(a => a.AdresseID);
+            Property(a => a.codePostal).HasColumnType("varchar").HasMaxLength(5);
+            Property(a => a.nomRue).HasColumnType("varchar").HasMaxLength(150);
+            Property(a => a.numRue).HasColumnType("varchar").HasMaxLength(10);
+            Property(a => a.ville).HasColumnType("varchar").HasMaxLength(80);
+            Property(a => a.pays).HasColumnType("varchar").HasMaxLength(80);
+        }
+    }
+}
