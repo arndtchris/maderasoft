27a864042281cb5594ddc81888449ed0047e13b0
chrisarndt <arndt_c@ymail.com>
2016-10-30 11:16:04 +0100

    Ajout d'une classe AdresseRepository.
    Les classes Repository permettent notamment d'override les méthodes Insert, Delete, Update...
    Pour implémenter une logique non métier comme la mise à jour d'une date de connectionn déconnection...

---
+++ Madera.Data/Repositories/AdresseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Madera.Data.Infrastructure;
+using Madera.Model;
+
+namespace Madera.Data.Repositories
+{
+    public class AdresseRepository : RepositoryBase<Adresse>, IAdresseRepository
+    {
+        public AdresseRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+
+    }
+
+    public interface IAdresseRepository : IRepository<Adresse>
+    {
+        
+    }
+}
