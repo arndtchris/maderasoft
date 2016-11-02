b6ad90dc208eb913b8d471e3eaac44259f3c8215
chrisarndt <arndt_c@ymail.com>
2016-11-02 21:31:22 +0100

    Définition d'un DTO utilisé pour l'affichage des adresses

---
+++ MaderaSoft/Models/AdresseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaderaSoft.Models
+{
+    public class AdresseModel
+    {
+        public int AdresseID { get; set; }
+        public string numRue { get; set; }
+        public string nomRue { get; set; }
+        public string codePostal { get; set; }
+        public string ville { get; set; }
+        public string pays { get; set; }
+    }
+}
