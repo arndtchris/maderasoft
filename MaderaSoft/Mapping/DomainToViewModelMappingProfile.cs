c40e4b54d9181437bdf864f7c1e4ae412977acd3
chrisarndt <arndt_c@ymail.com>
2016-11-02 21:30:53 +0100

    Configuration d'AutoMapper, ces classes peuvent permettre de mapper un DTO dont le nom de ses attributs ne correspondent pas avec le plain objet dans le projet Model

---
+++ MaderaSoft/Mapping/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Madera.Model;
+using MaderaSoft.Models;
+
+namespace MaderaSoft.Mapping
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            CreateMap<Adresse, AdresseModel>();
+        }
+    }
+}
