c40bf7b72d9c17fa203722b641583ce2d7b50d03
chrisarndt <arndt_c@ymail.com>
2016-11-02 21:32:24 +0100

    Définition du controller AdresseController et de la vue Index.cshtml permettant de lister les adresses en bdd

---
+++ MaderaSoft/Controllers/AdresseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using Madera.Model;
+using Madera.Service;
+using MaderaSoft.Models;
+
+namespace MaderaSoft.Controllers
+{
+    public class AdresseController : Controller
+    {
+        private readonly IAdresseService adresseService;
+
+        public AdresseController(IAdresseService adresseService)
+        {
+            this.adresseService = adresseService;
+        }
+
+        // GET: Adresse
+        public ActionResult Index()
+        {
+            IEnumerable<AdresseModel> adresseModels = Mapper.Map<IEnumerable<Adresse>, IEnumerable<AdresseModel>>(adresseService.GetAdresses().ToList());
+
+            return View(adresseModels);
+        }
+    }
+}
