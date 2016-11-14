b87d038cf36673f3633e91ba86630e530d3b25f7
chrisarndt <arndt_c@ymail.com>
2016-11-14 14:20:49 +0100

    Adresse : ajout d'un formulaire de création d'adresse en fonction d'un model AdresseDTO

--- MaderaSoft/Models/AdresseModel.cs
+++ MaderaSoft/Models/AdresseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using MaderaSoft.Models.Bootstrap;
@@ -9,10 +10,15 @@ namespace MaderaSoft.Models
     public class AdresseDTO
     {
         public int AdresseID { get; set; }
+        [DisplayName("Numéro")]
         public string numRue { get; set; }
+        [DisplayName("Nom")]
         public string nomRue { get; set; }
+        [DisplayName("Code postal")]
         public string codePostal { get; set; }
+        [DisplayName("Ville")]
         public string ville { get; set; }
+        [DisplayName("Pays")]
         public string pays { get; set; }
 
         public AdresseDTO()
