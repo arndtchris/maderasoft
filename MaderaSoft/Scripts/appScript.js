03196ddc25f5e23251b1c11b039180afd1292650
chrisarndt <arndt_c@ymail.com>
2016-11-14 14:12:41 +0100

    Ajout d'une feuille js pour la solution.
    Les boutons avec la classe "editModal" permettent de déclancher une fenêtre modale avec un formulaire de saisie/validation

---
+++ MaderaSoft/Scripts/appScript.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    $('.editModal').bind('click', function (event) {
+        event.preventDefault();
+        $.ajax({
+            method: "GET",
+            url: $(event.currentTarget).attr('href'),
+        })
+        .done(function (data) {
+            $("#editModal .modal-content").append(data);
+            $("#editModal").modal("show");
+        })
+        .fail(function (data) {
+            console.log("fail");
+        });
+    });
+
+    // Le contenu de la modal est chargé en Ajax 
+    // lorsque celle-ci est refermée il faut la vider, sinon les différents contenus chargés s'enchaînent
+    $(".modal").on("hidden.bs.modal", function () {
+        $("#editModal .modal-content").html("");
+    });
+
+});
