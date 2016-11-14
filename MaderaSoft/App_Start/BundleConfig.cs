abe3f456a01166ce584b4e85a121e6855e7f833a
chrisarndt <arndt_c@ymail.com>
2016-11-14 14:15:38 +0100

    Ajout de la nouvelle feuille js dans l'appel des ressources de l'application

--- MaderaSoft/App_Start/BundleConfig.cs
+++ MaderaSoft/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace MaderaSoft
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/appScript.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
