b691f267c159d8d6859efd57320155d943ad4869
chrisarndt <arndt_c@ymail.com>
2016-11-14 14:19:03 +0100

    Ajout d'un model et d'une vue partielle pour générer des modales bootstrap

---
+++ MaderaSoft/Models/Bootstrap/BootstrapModalModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace MaderaSoft.Models.Bootstrap
+{
+    public class BootstrapModalModel
+    {
+        public string formulaireUrl { get; set; }
+
+        public Object objet { get; set; }
+
+        public int idToDelete { get; set; }
+
+        public string typeObjet { get; set; }
+
+        //public string typeObjet { get; set; }
+
+        public string message { get; set; }
+
+        public string titreModal { get; set; }
+
+        public BootstrapModalModel()
+        {
+            objet = new object();
+        }
+
+    }
+}
