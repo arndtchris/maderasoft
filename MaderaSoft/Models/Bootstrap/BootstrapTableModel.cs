414a60e298dd4f47d038c549a16a686f995f42af
chrisarndt <arndt_c@ymail.com>
2016-11-14 14:17:12 +0100

    BootstrapTable : ajout d'un boolean pour gérer l'affichage ou non des actions UD dans le tableau

--- MaderaSoft/Models/Bootstrap/BootstrapTableModel.cs
+++ MaderaSoft/Models/Bootstrap/BootstrapTableModel.cs
@@ -8,10 +8,15 @@ namespace MaderaSoft.Models.Bootstrap
     public class BootstrapTableModel
     {
         public List<List<string>> lesLignes { get; set; }
+        public bool avecActionCrud { get; set; }
+        public string typeObjet { get; set; }
 
         public BootstrapTableModel()
         {
             lesLignes = new List<List<string>>();
+
+            //ToDo : gérer ce boolean en fonction des droits de l'utilisateur connecté
+            avecActionCrud = true;
         }
     }
 }
