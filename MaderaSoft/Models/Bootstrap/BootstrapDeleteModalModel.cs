b50f292dddbf963b0e07490f0307ff78d2f4bce8
chrisarndt <arndt_c@ymail.com>
2016-11-14 14:18:18 +0100

    Ajout d'un model et d'une vue partielle pour demander la confirmation d'une suppression

---
+++ MaderaSoft/Models/Bootstrap/BootstrapDeleteModalModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaderaSoft.Models.Bootstrap
+{
+    public class BootstrapDeleteModalModel
+    {
+        public int idToDelete { get; set; }
+
+        public string typeObjet { get; set; }
+
+        public string message { get; set; }
+
+        public BootstrapDeleteModalModel()
+        {
+            
+        }
+    }
+}
