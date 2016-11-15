24f20f79e93a2a534e2d754705a6823f6b8dba7d
chrisarndt <arndt_c@ymail.com>
2016-11-15 11:41:40 +0100

    AdresseDTO : ajout des méta-données qui vont être utilisées côté serveur et côté client (à l'aide de jquery validate unobtrusive).

--- MaderaSoft/Models/AdresseModel.cs
+++ MaderaSoft/Models/AdresseModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using MaderaSoft.Models.Bootstrap;
@@ -10,15 +11,21 @@ namespace MaderaSoft.Models
     public class AdresseDTO
     {
         public int AdresseID { get; set; }
-        [DisplayName("Numéro")]
+        [DisplayName("Numéro du logement")]
+        [Required(ErrorMessage = "Le numéro du logement est obligatoire.")]
         public string numRue { get; set; }
-        [DisplayName("Nom")]
+        [DisplayName("Nom de la rue")]
+        [Required(ErrorMessage = "Le nom de la rue est obligatoire.")]
         public string nomRue { get; set; }
         [DisplayName("Code postal")]
+        [Required(ErrorMessage = "Le code postal est obligatoire.")]
+        [RegularExpression("^(F-)?((2[A|B])|[0-9]{2})[0-9]{3}$", ErrorMessage = "Le format du code postal n'est pas valide")]
         public string codePostal { get; set; }
         [DisplayName("Ville")]
+        [Required(ErrorMessage = "La ville est obligatoire.")]
         public string ville { get; set; }
         [DisplayName("Pays")]
+        [Required(ErrorMessage = "Le pays est obligatoire.")]
         public string pays { get; set; }
 
         public AdresseDTO()
