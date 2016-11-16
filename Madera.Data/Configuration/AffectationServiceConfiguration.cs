0f66edb7d5af2190f7b0e5278f78e1afb3326149
chrisarndt <arndt_c@ymail.com>
2016-11-16 22:00:55 +0100

    AffectationService : première implémentation

---
+++ Madera.Data/Configuration/AffectationServiceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using Madera.Model;
+
+namespace Madera.Data.Configuration
+{
+    public class AffectationServiceConfiguration : EntityTypeConfiguration<AffectationService>
+    {
+        public AffectationServiceConfiguration()
+        {
+            ToTable("AffectationService");
+            HasKey<int>(a => a.AffectationServiceID);
+            Property<bool>(a => a.isPrincipal);
+
+        }
+    }
+}
---
+++ Madera.Data/Repositories/AffectationServiceRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Madera.Data.Infrastructure;
+using Madera.Model;
+
+namespace Madera.Data.Repositories
+{
+    public class AffectationServiceRepository : RepositoryBase<AffectationService>, IAffectationServiceRepository
+    {
+        public AffectationServiceRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+    }
+
+    public interface IAffectationServiceRepository : IRepository<AffectationService>
+    {
+        
+    }
+
+}
