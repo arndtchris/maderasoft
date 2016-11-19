6524e5fe55886bd1b410113fc97f8add6295c75e
chrisarndt <arndt_c@ymail.com>
2016-11-19 19:18:48 +0100

    Service : ajout d'une classe de paramétrage. Elle permet de définir les actions à renseigner en traçabilité.

---
+++ Madera.Service/Parametres.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Madera.Service
+{
+    public static class Parametres
+    {
+        public enum Action
+        {
+            Connection,
+            Deconnection,
+            Creation,
+            Modification,
+            SuppressionLogique,
+            Suppression
+        }
+    }
+}
