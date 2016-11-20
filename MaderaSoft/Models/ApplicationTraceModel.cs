a544b4a499f12d381aefe2ce692780c17d4fa41e
chrisarndt <arndt_c@ymail.com>
2016-11-20 18:54:36 +0100

    Définitio d'un objet ApplicationTraceDTO

---
+++ MaderaSoft/Models/ApplicationTraceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaderaSoft.Models
+{
+    public class ApplicationTraceDTO
+    {
+        public string utilisateur { get; set; }
+        public DateTime date { get; set; }
+        public string action { get; set; }
+        public string description { get; set; }
+
+        public ApplicationTraceDTO()
+        {
+
+        }
+
+    }
+}
