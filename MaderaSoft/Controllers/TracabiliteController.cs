3f48cbb958f5248e2b86ec83524f3ef513b6c7c6
chrisarndt <arndt_c@ymail.com>
2016-11-20 18:53:23 +0100

    Ajout d'un controller pour la tracabilité de l'application

---
+++ MaderaSoft/Controllers/TracabiliteController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using Madera.Model;
+using Madera.Service;
+using MaderaSoft.Models;
+using MaderaSoft.Models.Bootstrap;
+
+namespace MaderaSoft.Controllers
+{
+    public class TracabiliteController : Controller
+    {
+        private readonly IApplicationTraceService appTraceService;
+
+        public TracabiliteController(IApplicationTraceService applicationTraceService)
+        {
+            this.appTraceService = applicationTraceService;
+        }
+
+        // GET: Tracabilite
+        public ActionResult Index()
+        {
+            BootstrapTableModel modelOut = new BootstrapTableModel();
+            List<ApplicationTraceDTO> lesTraces = Mapper.Map<List<ApplicationTrace>, List<ApplicationTraceDTO>>(appTraceService.getAll().ToList());
+
+            modelOut.avecActionCrud = false;
+            modelOut.lesLignes.Add(new List<string> { "Utilisateur", "Date", "Action", "Description"});
+            
+
+            foreach (ApplicationTraceDTO appTraceDTO in lesTraces)
+            {
+                modelOut.lesLignes.Add(new List<string> { appTraceDTO.utilisateur, appTraceDTO.date.ToString(), appTraceDTO.action, appTraceDTO.description});
+            }
+
+
+            return View(modelOut);
+        }
+    }
+}
