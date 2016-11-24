f3964ca64609fab8f55235be00b10e4185b4c34a
Mickael <mickael.cimbaluria@gmail.com>
2016-11-24 20:55:48 +0100

    Création du modèle ManageQuoteModel

---
+++ MaderaSoft/Models/ManageQuoteModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MaderaSoft.Models.Bootstrap;
+using Madera.Model;
+
+namespace MaderaSoft.Models
+{
+    public class ManageQuoteViewModel
+    {
+
+        public class DevisFactureDTO
+        {
+            public int id { get; set; }
+            [DisplayName("Numéro de devis")]
+            public Boolean isSigned { get; set; }
+            [DisplayName("Devis signé")]
+            public Boolean isDeleted { get; set; }
+            [DisplayName("Devis supprimé")]
+            public virtual Projet projet { get; set; }
+            [DisplayName("Numéro du projet")]
+            public virtual Employe employe { get; set; }
+
+    public DevisFactureDTO()
+            {
+
+            }
+        }
+
+        public BootstrapTableModel tableauQuote { get; set; }
+
+
+        public ManageQuoteViewModel()
+        {
+            tableauQuote = new BootstrapTableModel();
+        }
+    }
+}
