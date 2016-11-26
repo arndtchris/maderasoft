f7eb5326c2aa26535c180d63409fb996d87e097a
chrisarndt <arndt_c@ymail.com>
2016-11-26 22:33:29 +0100

    Changement de la configuration de la migration pour quelle corresponde à notre architecture

--- MaderaSoft/Migrations/Configuration.cs
+++ MaderaSoft/Migrations/Configuration.cs
@@ -5,27 +5,27 @@ namespace MaderaSoft.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<MaderaSoft.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Madera.Data.MaderaEntities>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(MaderaSoft.Models.ApplicationDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
+        //protected override void Seed(MaderaSoft.Models.ApplicationDbContext context)
+        //{
+        //    //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+        //    //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+        //    //  to avoid creating duplicate seed data. E.g.
+        //    //
+        //    //    context.People.AddOrUpdate(
+        //    //      p => p.FullName,
+        //    //      new Person { FullName = "Andrew Peters" },
+        //    //      new Person { FullName = "Brice Lambson" },
+        //    //      new Person { FullName = "Rowan Miller" }
+        //    //    );
+        //    //
+        //}
     }
 }
