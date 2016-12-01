81a52e179cd2a8c78836579ec16f393e972e05b8
chrisarndt <arndt_c@ymail.com>
2016-12-01 18:17:09 +0100

    Création d'un DTO utilisateur

---
+++ MaderaSoft/Models/DTO/UtilisateurDTOs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaderaSoft.Models.DTO
+{
+    public class UtilisateurDTO
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public DateTime dCreation { get; set; }
+        public DateTime dConnexion { get; set; }
+        public bool isActive { get; set; }
+        public bool isDeleted { get; set; }
+
+        public string utilisateurActif()
+        {
+            if (this.isActive)
+                return "Actif";
+            else
+                return "Archivé";
+        }
+
+        public string utilisateurSupprime()
+        {
+            if (this.isDeleted)
+                return "Supprimé";
+            else
+                return "";
+        }
+
+        public UtilisateurDTO()
+        {
+
+        }
+    }
+}
