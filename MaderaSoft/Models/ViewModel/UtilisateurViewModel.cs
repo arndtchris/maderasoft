2bd9efd75a7e5521014dfdb8ad51f388cddc3941
chrisarndt <arndt_c@ymail.com>
2016-12-01 18:18:02 +0100

    Création d'un ViewModel pour la page index Utilisateur

---
+++ MaderaSoft/Models/ViewModel/UtilisateurViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MaderaSoft.Models.DTO;
+
+namespace MaderaSoft.Models.ViewModel
+{
+    public class UtilisateurIndexViewModel
+    {
+        public BootstrapTableViewModel tableauAdresses { get; set; }
+
+        public UtilisateurDTO nouvelUtilisateur { get; set; }
+
+        public UtilisateurIndexViewModel()
+        {
+            nouvelUtilisateur = new UtilisateurDTO();
+            tableauAdresses = new BootstrapTableViewModel();
+        }
+    }
+}
