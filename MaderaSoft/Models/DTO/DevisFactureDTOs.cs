8847f48fdf625ef0b633c3275b363b0383e34f6a
Mickael <mickael.cimbaluria@gmail.com>
2016-12-01 20:12:26 +0100

    Séparation DTO et ViewModel

---
+++ MaderaSoft/Models/DTO/DevisFactureDTOs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Madera.Model;
+using MaderaSoft.Models.ViewModel;
+
+namespace MaderaSoft.Models.DTO
+{
+    public class DevisFactureDTO
+    {
+
+            [DisplayName("Numéro Devis")]
+            public int id { get; set; }
+            [DisplayName("Devis signé")]
+            public Boolean isSigned { get; set; }
+            [DisplayName("Devis supprimé")]
+            public Boolean isDeleted { get; set; }
+            [DisplayName("Numéro projet")]
+            public virtual Projet projet { get; set; }
+            [DisplayName("Référent")]
+            public virtual Employe employe { get; set; }
+
+        public DevisFactureDTO()
+        {
+        }
+    }
+}
---
+++ MaderaSoft/Models/ViewModel/DevisFactureViewModels.cs
@@ -0,0 +1,23 @@
+﻿using MaderaSoft.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaderaSoft.Models.ViewModel
+{
+    public class DevisFactureIndexViewModel
+    {
+        public BootstrapTableViewModel tableauDevisFactures{ get; set; }
+
+        public DevisFactureDTO nouveaudevis { get; set; }
+
+        public DevisFactureIndexViewModel()
+        {
+            nouveaudevis = new DevisFactureDTO();
+            tableauDevisFactures = new BootstrapTableViewModel();
+        }
+    }
+}
