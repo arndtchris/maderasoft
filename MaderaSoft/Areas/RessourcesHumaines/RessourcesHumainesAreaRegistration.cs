aa905a19011c4c728741206d3748caa632099bb2
chrisarndt <arndt_c@ymail.com>
2016-12-02 18:41:26 +0100

    Ajout d'une zone "Area" regroupant toutes les fonctionnalités liées au service des ressources humaines

---
+++ MaderaSoft/Areas/RessourcesHumaines/RessourcesHumainesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MaderaSoft.Areas.RessourcesHumaines
+{
+    public class RessourcesHumainesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "RessourcesHumaines";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "RessourcesHumaines_default",
+                "RessourcesHumaines/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
