23c3757c3726c4d05c12ed27465cc8f27d09b64d
chrisarndt <arndt_c@ymail.com>
2016-12-02 18:41:53 +0100

    RessourcesHumaines : création d'un controller EmployeController

---
+++ MaderaSoft/Areas/RessourcesHumaines/Controllers/EmployeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Madera.Service;
+
+namespace MaderaSoft.Areas.RessourcesHumaines.Controllers
+{
+    public class EmployeController : Controller
+    {
+        //private readonly EmployeService employeService;
+
+        // GET: RessourcesHumaines/Employe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
