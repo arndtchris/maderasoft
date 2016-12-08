410b42b8dd5ae5990fac1aa2fc9775c97518f37a
chrisarndt <arndt_c@ymail.com>
2016-12-08 19:48:42 +0100

    DTO/PersonneDTOs : ajout d'un DTO de l'objet Personne à utiliser lors de la création de nouvelles entrées en bdd

---
+++ MaderaSoft/Models/DTO/PersonneDTOs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaderaSoft.Models.DTO
+{
+    public class PersonneDTO
+    {
+        public int id { get; set; }
+        [DisplayName("Civilité")]
+        [Required(ErrorMessage = "Veuillez renseigner une civilité")]
+        public string civ { get; set; }
+        [DisplayName("Nom")]
+        [Required(ErrorMessage = "Veuillez renseigner un nom")]
+        public string nom { get; set; }
+        [DisplayName("Prénom")]
+        [Required(ErrorMessage = "Veuillez renseigner un prénom")]
+        public string prenom { get; set; }
+        [DisplayName("Email")]
+        //[RegularExpression("^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$", ErrorMessage = "Cette adresse email n'est as valide")]
+        public string email { get; set; }
+        [DisplayName("Téléphone portable")]
+        public string tel1 { get; set; }
+        [DisplayName("Téléphone fixe")]
+        public string tel2 { get; set; }
+        public AdresseDTO adresse { get; set; }
+        public List<SelectListItem> lesCivilites { get; set; }
+
+        public PersonneDTO()
+        {
+            adresse = new AdresseDTO();
+            lesCivilites = new List<SelectListItem> {
+                new SelectListItem {Value = "0",Text = "--- Sélectionnez ---" },
+                new SelectListItem {Value = "1",Text = "Madame" },
+                new SelectListItem {Value = "2",Text = "Monsieur" }
+            };
+
+        }
+    }
+}
