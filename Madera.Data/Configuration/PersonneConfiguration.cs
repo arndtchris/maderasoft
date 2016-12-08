53885a2673aac7f36eee490549fe32eeb46c576e
chrisarndt <arndt_c@ymail.com>
2016-12-08 19:31:56 +0100

    PersonneConfiguration : mise en place d'une relation 0..1 de Personne vers Utilisateur et Adresse

--- Madera.Data/Configuration/PersonneConfiguration.cs
+++ Madera.Data/Configuration/PersonneConfiguration.cs
@@ -13,6 +13,10 @@ namespace Madera.Data.Configuration
         {
             ToTable("Personne");
             HasKey<int>(a => a.id);
+
+            HasOptional<Adresse>(x => x.adresse);
+            HasOptional<Utilisateur>(x => x.utilisateur);
+
             Property(a => a.civ).HasColumnType("varchar").HasMaxLength(3);
             Property(a => a.nom).HasColumnType("varchar").HasMaxLength(80);
             Property(a => a.prenom).HasColumnType("varchar").HasMaxLength(80);
