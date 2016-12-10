1ad5f61a8bc7c1f6244ed6aea6c29bdcb06fe2ca
chrisarndt <arndt_c@ymail.com>
2016-12-10 11:29:13 +0100

    Mise en place du thème AdminLTE pour la partie applicative de MaderaSoft

---
+++ MaderaSoft/Scripts/Admin/appScript.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+    $('.editModal').bind('click', function (event) {
+        event.preventDefault();
+        $.ajax({
+            method: "GET",
+            url: $(event.currentTarget).attr('href'),
+        })
+        .done(function (data) {
+            $("#editModal .modal-content").append(data);
+            $.validator.unobtrusive.parse("form");
+            $("#editModal").modal("show");
+        })
+        .fail(function (data) {
+            console.log("fail");
+        });
+    });
+
+    // Le contenu de la modal est chargé en Ajax 
+    // lorsque celle-ci est refermée il faut la vider, sinon les différents contenus chargés s'enchaînent
+    $(".modal").on("hidden.bs.modal", function () {
+        $("#editModal .modal-content").html("");
+    });
+
+    $(".alert").fadeOut(6000);
+
+    $.validator.unobtrusive.parse("form");
+
+});
