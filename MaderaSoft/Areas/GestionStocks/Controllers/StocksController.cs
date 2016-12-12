6d8782bd74564b6b60f56a1b4d6fd486fd411d11
Mickael <mickael.cimbaluria@gmail.com>
2016-12-12 12:03:57 +0100

    Stockcontroller

---
+++ MaderaSoft/Areas/GestionStocks/Controllers/StocksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaderaSoft.Areas.GestionStocks.Controllers
+{
+    public class StocksController : Controller
+    {
+        // GET: GestionStocks/Stocks
+        public class StocksDTO
+        {
+            [DisplayName("Numéro Devis")]
+            public int id { get; set; }
+            [DisplayName("Devis signé")]
+            public Boolean isSigned { get; set; }
+            [DisplayName("Devis supprimé")]
+            public Boolean isDeleted { get; set; }
+            [DisplayName("Numéro projet")]
+            public virtual Projet projet { get; set; }
+            [DisplayName("Référent")]
+            public virtual Employe employe { get; set; }
+
+            public StocksDTO()
+            {
+            }
+        }
+    }
+}
