ab80e510e3dd05f7ade1f81ec87c13b887bd10f1
Mickael <mickael.cimbaluria@gmail.com>
2016-12-12 11:16:29 +0100

    Ajout constructeur projet

--- Madera.Models/Models/Projet.cs
+++ Madera.Models/Models/Projet.cs
@@ -18,9 +18,17 @@ namespace Madera.Model
         public virtual Adresse adresse { get; set; }
         public virtual Employe referent { get; set; }
 
-        public Projet()
+        public Projet(int i, string l, float pHT, float pTTC, bool deleted, bool paid, Personne cl, Adresse ad, Employe referent)
         {
-
+            this.id = i;
+            this.libe = l;
+            this.prixHT = pHT;
+            this.prixTotalTTC = pTTC;
+            this.isDeleted = deleted;
+            this.isPaid = paid;
+            this.client = cl;
+            this.adresse = ad;
+            this.referent = referent;
         }
     }
 }
