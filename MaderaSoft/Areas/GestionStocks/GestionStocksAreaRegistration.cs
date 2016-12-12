7fc6f9f30c1c0803e7f519cf64b7cc253188589b
Mickael <mickael.cimbaluria@gmail.com>
2016-12-12 11:17:58 +0100

    Ajout GestionStock Area + csproj

---
+++ MaderaSoft/Areas/GestionStocks/GestionStocksAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MaderaSoft.Areas.GestionStocks
+{
+    public class GestionStocksAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GestionStocks";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GestionStocks_default",
+                "GestionStocks/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
