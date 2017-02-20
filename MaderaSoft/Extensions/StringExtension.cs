c9b074dcdc8937a4ae223e3af1c4b1ca9b0cd1a6
chrisarndt <arndt_c@ymail.com>
2017-02-20 09:56:05 +0100

    Ajout de l'extension ToUpperFirst() qui permet de mettre en majuscule la première lettre d'une chaîne de caractères

---
+++ MaderaSoft/Extensions/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace System
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Permet de mettre en majuscule la première lettre d'une chaîne de caractères
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToUpperFirst(this string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                throw new ArgumentException("La chaîne de carctères est null");
+            return str.First().ToString().ToUpper() + str.Substring(1);
+        }
+    }
+}
