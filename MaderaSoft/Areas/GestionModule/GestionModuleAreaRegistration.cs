e54fbc63316ef24ae8b911ebc9dd0e7d34bfdfd0
Mickael <mickael.cimbaluria@gmail.com>
2017-03-04 17:23:36 +0100

    Ajout de l'Area pour les modules

---
+++ MaderaSoft/Areas/GestionModule/GestionModuleAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MaderaSoft.Areas.GestionModule
+{
+    public class GestionModuleAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GestionModule";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GestionModule_default",
+                "GestionModule/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
