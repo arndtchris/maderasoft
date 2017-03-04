11a64644869d252a970e1b886e8b2b9baa90b52e
Mickael <mickael.cimbaluria@gmail.com>
2017-03-04 17:22:28 +0100

    Création du service "ModuleService"

---
+++ Madera.Service/ModuleService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Madera.Data.Infrastructure;
+using Madera.Data.Repositories;
+using Madera.Model;
+
+namespace Madera.Service
+{
+    public class ModuleService : IModuleService
+    {
+
+        private readonly IModuleRepository _moduleRepository;
+        private readonly IApplicationTraceService _applicationTraceService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ModuleService(IModuleRepository moduleRepository, IUnitOfWork unitOfWork, IApplicationTraceService applicationTraceService)
+        {
+            this._moduleRepository = moduleRepository;
+            this._applicationTraceService = applicationTraceService;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Create(Module module)
+        {
+            _applicationTraceService.create(new ApplicationTrace
+            {
+                action = Parametres.Action.Creation.ToString(),
+            });
+
+            _moduleRepository.Insert(module);
+        }
+
+        public void Delete(int id)
+        {
+            _applicationTraceService.create(new ApplicationTrace
+            {
+                action = Parametres.Action.Suppression.ToString(),
+                description = String.Format("Supression du composant_id = {0}", id),
+            });
+            _moduleRepository.Delete(x => x.id == id);
+        }
+    
+
+        public Module Get(int id)
+        {
+            return _moduleRepository.GetById(id);
+        }
+
+        public IEnumerable<Module> DonneTous()
+        {
+            return _moduleRepository.GetAll();
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Module module)
+        {
+            _applicationTraceService.create(new ApplicationTrace
+            {
+                action = Parametres.Action.Creation.ToString(),
+            });
+
+            _moduleRepository.Update(module);
+        }
+    }
+
+    public interface IModuleService : IService<Module>
+    {
+        void Create(Module module);
+        void Update(Module module);
+        void Save();
+        void Delete(int id);
+
+    }
+}
