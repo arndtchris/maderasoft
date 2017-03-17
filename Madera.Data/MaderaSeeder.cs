b2549e555a15ad0600a0b40529be7f4515aee9df
Mickael <mickael.cimbaluria@gmail.com>
2017-03-17 10:03:24 +0100

    Ajout de fournisseurs dans Seeder

--- Madera.Data/MaderaSeeder.cs
+++ Madera.Data/MaderaSeeder.cs
@@ -23,6 +23,7 @@ namespace Madera.Data
             SeedTEmployes().ForEach(h => context.TEmployes.Add(h));
             SeedGammes().ForEach(i => context.Gamme.Add(i));
             SeedTModule().ForEach(j => context.TModule.Add(j));
+            SeedPersonne().ForEach(j => context.Personnes.Add(j));
             //SeedModules().ForEach(j => context.Module.Add(j));
 
             context.Commit();
@@ -70,24 +71,26 @@ namespace Madera.Data
                 new Gamme
                 {
                     id = 1,
-                    libe = "Essentiel",
+                    libe = "Basic",
                     pourcentageGamme = 10
                 },
                 new Gamme
                 {
                     id = 2,
-                    libe = "Intermédiaire",
+                    libe = "Timber",
                     pourcentageGamme = 20
                 },
                 new Gamme
                 {
                     id = 3,
-                    libe = "Luxe",
+                    libe = "Prestige",
                     pourcentageGamme = 30
                 },
 
             };
         }
+
+
 
         private static List<Service> SeedServices()
         {
@@ -131,6 +134,123 @@ namespace Madera.Data
                 new TEmploye
                 {
                     libe = "DSI"
+                }
+
+            };
+        }
+
+        private static List<Personne> SeedPersonne()
+        {
+            //Création d'adresses pour les fournisseurs
+            //Adresse Les portes du paradis
+            Adresse adresse1 = new Adresse();
+            adresse1.id = 100;
+            adresse1.nomRue = "rue des petits champs";
+            adresse1.numRue = "21";
+            adresse1.pays = "France";
+            adresse1.ville = "Nancy";
+            adresse1.codePostal = "54000";
+
+            //Création d'adresses pour les fournisseurs
+            //Adresse Placards de rêve
+            Adresse adresse2 = new Adresse();
+            adresse2.id = 101;
+            adresse2.nomRue = "Zone industrielle du Beau bois";
+            adresse2.numRue = "32";
+            adresse2.pays = "France";
+            adresse2.ville = "Ludres";
+            adresse2.codePostal = "54710";
+
+            //Création d'adresses pour les fournisseurs
+            //Adresse Top du bois
+            Adresse adresse3 = new Adresse();
+            adresse3.id = 102;
+            adresse3.nomRue = "rue des arbres";
+            adresse3.numRue = "46";
+            adresse3.pays = "France";
+            adresse3.ville = "Nancy";
+            adresse3.codePostal = "54000";
+
+            //Création de users pour les fournisseurs
+            //User Portes du paradis
+            Utilisateur user1 = new Utilisateur();
+            user1.id = 100;
+            user1.login = "lesportesduparadis";
+            user1.password = "123456";
+            user1.isActive = true;
+            user1.isDeleted = false;
+            user1.dConnexion = DateTime.Now;
+            user1.dCreation = DateTime.Now;
+            user1.isFirstConnexion = false;
+
+            //User Placards de rêve
+            Utilisateur user2 = new Utilisateur();
+            user2.id = 101;
+            user2.login = "placardsdereve";
+            user2.password = "123456";
+            user2.isActive = true;
+            user2.isDeleted = false;
+            user2.dConnexion = DateTime.Now;
+            user2.dCreation = DateTime.Now;
+            user2.isFirstConnexion = false;
+
+            //User top du bois
+            Utilisateur user3 = new Utilisateur();
+            user3.id = 102;
+            user3.login = "letopdubois";
+            user3.password = "123456";
+            user3.isActive = true;
+            user3.isDeleted = false;
+            user3.dConnexion = DateTime.Now;
+            user3.dCreation = DateTime.Now;
+            user3.isFirstConnexion = false;
+
+            return new List<Personne>
+            {
+                new Personne
+                {
+                    id = 100,
+                    civ = "",
+                    nom = "Les portes du paradis",
+                    prenom = "",
+                    isFournisseur = true,
+                    isClient = false,
+                    isDeleted = false,
+                    email = "lesportesduparadis@contact.fr",
+                    tel1 = "0393227470",
+                    tel2 = "",
+                    adresse = adresse1,
+                    utilisateur = user1   
+                },
+                new Personne
+                {
+                    id = 101,
+                    civ = "",
+                    nom = "Placards de rêve",
+                    prenom = "",
+                    isFournisseur = true,
+                    isClient = false,
+                    isDeleted = false,
+                    email = "placardsdereve@contact.fr",
+                    tel1 = "0372727470",
+                    tel2 = "",
+                    adresse = adresse2,
+                    utilisateur = user2
+                },
+                new Personne
+                {
+                    id = 102,
+                    civ = "",
+                    nom = "Top du bois",
+                    prenom = "",
+                    isFournisseur = true,
+                    isClient = false,
+                    isDeleted = false,
+                    email = "topdubois@contact.fr",
+                    tel1 = "0326123436",
+                    tel2 = "",
+                    adresse = adresse3,
+                    utilisateur = user3
                 }
 
             };
