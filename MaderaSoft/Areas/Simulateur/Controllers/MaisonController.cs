52cb39f05f2d5c9186bfa9232065379b34dd9b29
Thomas55170 <tberthemin@yahoo.com>
2017-03-17 16:29:58 +0100

    Charge plan côté serveur

--- MaderaSoft/Areas/Simulateur/Controllers/MaisonController.cs
+++ MaderaSoft/Areas/Simulateur/Controllers/MaisonController.cs
@@ -115,12 +115,107 @@ namespace MaderaSoft.Areas.Simulateur.Controllers
         {
             try
             {
-                //List<ModuleDTO> lesModules = Mapper.Map<List<Module>, List<ModuleDTO>>(_moduleService.DonneTous().ToList());
-                //PlanDTO plan = Mapper.Map<Plan, PlanDTO>(_planService.Get(id));
-
                 Plan p = _planService.Get(id);
 
-                return Json(p);
+                //Création du nouveau plan
+                Plan planReturn = new Plan();
+
+                //Ajout des informations du plan par rapport à l'ancien
+                planReturn.largeur = p.largeur;
+                planReturn.longueur = p.longueur;
+                planReturn.nom = p.nom;
+                planReturn.id = p.id;
+
+                //Création des listes de modules et étages pour le nouveau objet Plan
+                List<PositionModule> listPositionModuleReturn = new List<PositionModule>();
+                List<Etage> listEtagesReturn = new List<Etage>();
+
+
+                //Calcul nombre de trait dans la grille (ex: larg:3 long:7 = trait:52)
+                int total = ( 1 + planReturn.largeur) * planReturn.longueur + ( 1 + planReturn.longueur) * planReturn.largeur;
+
+                //Calcul nombre de trait horizontal
+                int totalHorizontal = (1 + planReturn.largeur) * planReturn.longueur;
+                //Calcul nombre de trait vertical
+                int totalVerticale = (1 + planReturn.longueur) * planReturn.largeur;
+
+                // intialisation valeur de départ
+                int xGrille = 10;
+                int yGrille = 10;
+
+                // boucle sur les étages du plan
+                foreach (Etage etage in p.listEtages)
+                {
+                    //Création de l'objet Étage
+                    Etage etageTemp = new Etage();
+                    etageTemp.id = etage.id;
+
+                    for (int i = 0; i < totalHorizontal; i++)
+                    {
+                        //Création de l'objet PositionModule
+                        PositionModule pm = new PositionModule();
+
+                        int xAfter = xGrille + 40;
+                        xGrille = xGrille + 1;
+
+                        pm = etage.listPositionModule.FirstOrDefault(x => x.x1 == xGrille && x.x2 == xAfter && x.y1 == yGrille && x.y2 == yGrille);
+
+                        if (pm == null)
+                        {
+                            pm = new PositionModule();
+                            listPositionModuleReturn.Add(pm);
+                        }
+                        else
+                        {
+                            listPositionModuleReturn.Add(pm);
+                        }
+
+                        xGrille = xAfter;
+
+                        yGrille = yGrille + 40;
+                        xGrille = 10;
+                    }
+                    for (int i = 0; i < totalVerticale; i++)
+                    {
+                        //Création de l'objet PositionModule
+                        PositionModule pm = new PositionModule();
+
+                        //-----------------------------------------------------------------------------------------------------------------------------
+
+                        xGrille = 10;
+                        yGrille = 10;
+
+                        int yAfter = yGrille + 40;
+                        yGrille = yGrille + 1;
+
+                        pm = etage.listPositionModule.FirstOrDefault(x => x.x1 == xGrille && x.x2 == xGrille && x.y1 == yGrille && x.y2 == yAfter);
+
+                        if (pm == null)
+                        {
+                            pm = new PositionModule();
+                            listPositionModuleReturn.Add(pm);
+                        }
+                        else
+                        {
+                            //Ajout dans l'objet PositionModule dans la listPositionModule
+                            listPositionModuleReturn.Add(pm);
+                        }
+
+                        xGrille = xGrille + 40;
+                        yGrille = 10;
+
+                    }
+
+                    //Ajout de la liste des modules dans l'objet Étage
+                    etageTemp.listPositionModule = listPositionModuleReturn;
+                    //Ajout de l'objet Étage dans la liste des étages
+                    listEtagesReturn.Add(etageTemp);
+
+                }
+                //Ajout des étages dans le plan
+                planReturn.listEtages = listEtagesReturn;
+
+                return Json(planReturn);
             }
             catch(Exception e)
             {
